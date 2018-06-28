4e5cd09a15c8202acc982edc766485e070491177
CapacitorSet <capacitorset@gmail.com>
2018-06-28 21:01:45 +0200

    Use custom table in Events

--- src/components/events.js
+++ src/components/events.js
@@ -3,22 +3,14 @@ import moment from 'moment';
 
 import { connect } from 'react-redux';
 
+import Table from './table';
 
 import View from './view';
 import Flag from "react-flags";
 
 class Events extends Component {
-    renderTable() {
-        if(!this.props.events) {
-            return (
-                <div>Loading...</div>
-            );
-        }
-
-        const { events } = this.props;
-
-        // sort on time
-        return events
+    render() {
+        const data = this.props.events
             .filter(e => !("yara.rule" in e))
             .map(event => {
                 event.date = moment(event.date).utc();
@@ -26,53 +18,67 @@ class Events extends Component {
             })
             .sort((left, right) => right.date.diff(left.date))
             .slice(0, 10)
-            .map((event, i) => {
-                let message = (event.message || event.payload);
+            .map(e => {
+                let message = (e.message || e.payload);
 
-                if (event.category == 'ssh') {
-                    message = event['ssh.request-type'];
+                if (e.category == 'ssh') {
+                    message = e['ssh.request-type'];
                 }
 
-                return (
-                    <tr key={i}>
-                        <td>{event.date.fromNow()}</td>
-                        <td>{event.sensor}</td>
-                        <td>{event.category}</td>
-                        <td><Flag
-                            name={event['source.country.isocode']}
-                            basePath="images/flags"
-                            format="png"
-                            pngSize={16}
-                            shiny={false}
-                        />
-                        {event["source-ip"] } ({event["source-port"] })</td>
-                        <td>{event["destination-ip"] } ({event["destination-port"] })</td>
-                        <td>{message}</td>
-                    </tr>
-                );
+                return {
+                    date: e.date,
+                    sensor: e.sensor,
+                    category: e.category,
+                    source: {
+                        isocode: e['source.country.isocode'],
+                        ip: e["source-ip"],
+                        port: e["source-port"]
+                    },
+                    destination: {
+                        ip: e["destination-ip"],
+                        port: e["destination-port"]
+                    },
+                    message: message || "",
+                };
             });
-    }
-
-    render() {
-        const events = this.renderTable();
 
         return (
             <View title="Overview" subtitle="Events">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th className="header">Date</th>
-                            <th className="header">Sensor</th>
-                            <th className="header">Category</th>
-                            <th className="header">Source</th>
-                            <th className="header">Destination</th>
-                            <th className="header">Message</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { events }
-                    </tbody>
-                </table>
+                <Table headers={[{
+                    name: "Date",
+                    key: "date",
+                    renderer: date => date.fromNow()
+                }, {
+                    name: "Sensor",
+                    key: "sensor",
+                    isSearchable: true,
+                }, {
+                    name: "Category",
+                    key: "category",
+                    isSearchable: true,
+                }, {
+                    name: "Source",
+                    key: "source",
+                    isSearchable: true,
+                    renderer: ({isocode, ip, port}) => <span><Flag
+                        name={isocode}
+                        basePath="images/flags"
+                        format="png"
+                        pngSize={16}
+                        shiny={false}
+                    /> {ip} ({port})</span>,
+                    stringify: ({ip, port}) => ip + ":" + port,
+                }, {
+                    name: "Destination",
+                    key: "destination",
+                    isSearchable: true,
+                    renderer: ({ip, port}) => ip + ":" + port,
+                    stringify: ({ip, port}) => ip + ":" + port,
+                }, {
+                    name: "Message",
+                    key: "message",
+                    isSearchable: true,
+                }]} data={data} />
             </View>
         );
     }
